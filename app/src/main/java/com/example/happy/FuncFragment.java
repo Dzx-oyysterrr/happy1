b0d46618f48852a66b9f4106e7cd724560a38797
Your name <you@example.com>
2019-10-28 08:54:54 +0800

    hh

---
+++ app/src/main/java/com/example/happy/FuncFragment.java
@@ -0,0 +1,26 @@
+package com.example.happy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class FuncFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.frame_func,container);
+
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView tv = (TextView)getView().findViewById(R.id.funcTextView1);
+        tv.setText("这是功能页面");
+    }
+
+
+}
